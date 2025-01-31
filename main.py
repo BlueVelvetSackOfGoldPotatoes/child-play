01a8b168ded494fe7fe620af05bbe6d2d9320b56
Oscar <56079903+OscarNOW@users.noreply.github.com>
2025-01-31 19:14:23 +0000

    Actually fixed battleship and tictactoe guess parsing

--- main.py
+++ main.py
@@ -123,9 +123,14 @@ class TextPlayer(PlayerBase):
                 elif game.name in ["connectfour"]:
                     col = int(text_guess)
                     return col if 0 <= col < game.cols and game.board[0][col] == '.' else None
-                else:
+                elif game.name == "battleship":
                     row, col = map(int, text_guess.split())
                     return (row, col) if 0 <= row < game.board_size and 0 <= col < game.board_size and game.board[row][col] == "~" else None
+                elif game.name == "tictactoe":
+                    row, col = map(int, text_guess.split())
+                    return (row, col) if 0 <= row < game.board_size and 0 <= col < game.board_size and game.board[row][col] == " " else None
+                else:
+                    raise NotImplementedError
             except (ValueError, IndexError):
                 # If input parsing fails or doesn't meet the criteria, inform the player and allow another attempt
                 self.collect_message("Invalid input. Please enter the column number for ConnectFour or row and column numbers separated by a space for other games.")
