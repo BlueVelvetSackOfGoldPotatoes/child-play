6c9f0f3168d052790908a89bffb9fedef5a62e79
Oscar <56079903+OscarNOW@users.noreply.github.com>
2025-02-20 17:44:18 +0000

    Updated example

--- python_api_example.py
+++ python_api_example.py
@@ -6,34 +6,35 @@ from scripts_games.battleship import BattleShip
 import os
 import asyncio
 
+
 def clear():
-    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+async def ask(messages, prompt):
+    # Implement your own model here
+
+    clear()
+    for message in messages:
+        print(message)
+
+    return input(prompt)
+
 
 async def main():
-    async def ask(messages, prompt):
-        clear()
-        
-        for message in messages: print(message)
-
-        if len(game.moves) >= 2:
-            last_score = game.moves[len(game.moves) - 2]["score"]
-            print(f"last score: {last_score}")
-        
-        return input(prompt)
-    
     game = TwoPlayerGame(ConnectFour, ask)
 
-    # runs the game
-    # await game.make_turns(amount_of_turns=1)
+    await game.make_turns(1)
 
-    # runs random moves for the amount_of_turns 
-    # await game.random_moves(amount_of_turns=3)
+    await game.random_moves(3)
 
-    # runs the game until finish
-    await game.make_turns_until_finish()
-    
+    if not game.finished:
+        await game.make_turns_until_finish()
+
+    # game is finished
+
     clear()
-    
+
     print("moves", game.moves)
     # [
     #     {
@@ -43,14 +44,14 @@ async def main():
     #         score: 0-1
     #     }
     # ]
-    
+
     print(game.text_state)
-    
+
     print("winner", game.winner)
     # None (if game not finished), "tie", "custom", "opponent" or "random"
-    
+
     print("invalid attempts", game.invalid_attempts)
-    # number of invalid attempts by custom player
-    
+
+
 if __name__ == "__main__":
     asyncio.run(main())
